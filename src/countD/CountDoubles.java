e61cf3c00c3a239defb5d3309531abbd5349cca1
MykytiukSS <m.sergeysergeyevich@gmail.com>
2022-06-17 20:34:16 +0300

    main was created and tested prog

--- src/countD/CountDoubles.java
+++ src/countD/CountDoubles.java
@@ -2,32 +2,62 @@ package countD;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-
+import java.util.List;
 import java.util.Map;
 
-public class CountDoubles<E> {
+public class CountDoubles {
 
-	public void countD(ArrayList<E> list) {
-		Map<E, Integer> doubles = new HashMap<>();
-		int count = 1;
-		for (E e : list) {
+	public void countDString(List<String> stringList) {
+		Map<String, Integer> doubles = new HashMap<>();
+		int count = 0;
+		for (String e : stringList) {
 
-			for (E t : list) {
-				if (e == t || e.equals(t)) {
+			for (String t : stringList) {
+				if (e.equals(t)) {
 					count++;
 				}
 			}
-			if (count != 1) {
+			if (count > 1) {
 				doubles.put(e, count);
-				count = 1;
+				
 
 			}
+			count = 0;
 		}
 
 		System.out.println("Repeated next values:");
 		System.out.println();
-		for (Map.Entry<E, Integer> entry : doubles.entrySet()) {
-			E key = entry.getKey();
+		for (Map.Entry<String, Integer> entry : doubles.entrySet()) {
+			String key = entry.getKey();
+			Integer val = entry.getValue();
+
+			System.out.println(key + " - " + val + " times");
+			;
+		}
+
+	}
+	public void countDInteger(List<Integer> intList) {
+		Map<Integer, Integer> doubles = new HashMap<>();
+		int count = 0;
+		for (Integer e : intList) {
+
+			for (Integer t : intList) {
+				if (e == t) {
+					count++;
+				}
+			}
+			if (count > 1) {
+				doubles.put(e, count);
+				
+
+			}
+			count = 0;
+		}
+
+		System.out.println("Repeated next values:");
+		System.out.println();
+		for (Map.Entry<Integer, Integer> entry : doubles.entrySet()) {
+			Integer key = entry.getKey();
 			Integer val = entry.getValue();
 
 			System.out.println(key + " - " + val + " times");
---
+++ src/countD/Main.java
@@ -0,0 +1,22 @@
+package countD;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+	 List<String>stringList=new ArrayList<>(Arrays.asList("game","home","room","table","and","lamp","tree","home","car","and","tree","and"));
+	 List<Integer>intList=new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,3,5,6,4,8,7,6,1,6,7,5,5));
+	 
+	CountDoubles string = new CountDoubles();
+	CountDoubles integ = new CountDoubles();
+	
+	string.countDString(stringList);
+	string.countDInteger(intList);
+	
+
+	}
+
+}
